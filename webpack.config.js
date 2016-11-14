1d32eed363062f76318acf1a5f317a4a2e05d58d
zhouyuan <zhouyuan3118@163.com>
2016-11-14 17:28:19 +0800

    commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/**
+ * Created by zhou on 16/11/14.
+ */
+// webpack.config.js
+var path = require("path");
+var webpack = require('webpack');
+
+var publicPath = 'http://localhost:3000/';
+var hotMiddlewareScript = 'webpack-hot-middleware/client?reload=true';
+
+var devConfig = {
+    entry: {
+        page1: ['./page1', hotMiddlewareScript],
+    },
+    output: {
+        filename: './[name]/bundle.js',
+        path: path.resolve('../dist'),
+        publicPath: publicPath
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [{
+            test: /\.(png|jpg)$/,
+            loader: 'url?limit=8192&context=client&name=[path][name].[ext]'
+        }, {
+            test: /\.scss$/,
+            loader: 'style!css?sourceMap!resolve-url!sass?sourceMap'
+        }]
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+};
+
+module.exports = devConfig;
