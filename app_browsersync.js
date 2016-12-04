05f9595d1735f0156cecb5e550e21b84d0051cba
sunnoder <m18205253820@163.com>
2016-12-04 23:48:38 +0800

    record route response time

--- app_browsersync.js
+++ app_browsersync.js
@@ -49,6 +49,23 @@ var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
 app.use(morgan('combined', {stream: accessLogStream}))
 app.use(cors());
 
+//记录路由响应时间
+app.use(function (req, res, next) {
+    // 记录start time:
+    var exec_start_at = Date.now();
+    // 保存原始处理函数:
+    var _send = res.send;
+    // 绑定我们自己的处理函数:
+    res.send = function () {
+        // 发送Header:
+        res.set('X-Execution-Time', String(Date.now() - exec_start_at));
+        // 调用原始处理函数:
+        return _send.apply(res, arguments);
+    };
+    next();
+});
+
+
 app.locals.env = process.env.NODE_ENV || 'dev';
 app.locals.reload = false;
 
